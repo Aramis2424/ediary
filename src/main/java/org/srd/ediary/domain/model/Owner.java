b3a3ed54858725657d38993754446831a50e1dd4
Aramis2424 <srd2424@yandex.ru>
2024-11-12 22:35:59 +0300

    add getters

--- src/main/java/org/srd/ediary/domain/model/Owner.java
+++ src/main/java/org/srd/ediary/domain/model/Owner.java
@@ -3,10 +3,43 @@ package org.srd.ediary.domain.model;
 import java.time.LocalDate;
 
 public class Owner {
-    private Long id;
-    private String name;
-    private LocalDate birthDate;
-    private String login;
-    private String password;
-    private LocalDate createdDate;
+    private final Long id;
+    private final String name;
+    private final LocalDate birthDate;
+    private final String login;
+    private final String password;
+    private final LocalDate createdDate;
+
+    public Owner(String name, LocalDate birthDate, String login, String password) {
+        this.id = -1L;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.login = login;
+        this.password = password;
+        this.createdDate = LocalDate.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
 }
