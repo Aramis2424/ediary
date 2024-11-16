2230c5ba18304f5d1791755845f3f06c165ff406
Aramis2424 <srd2424@yandex.ru>
2024-11-16 16:23:05 +0300

    add id to owner infodto

--- src/main/java/org/srd/ediary/application/dto/OwnerInfoDTO.java
+++ src/main/java/org/srd/ediary/application/dto/OwnerInfoDTO.java
@@ -3,6 +3,7 @@ package org.srd.ediary.application.dto;
 import java.time.LocalDate;
 
 public record OwnerInfoDTO(
+        Long id,
         String name,
         LocalDate birthDate,
         String login,
