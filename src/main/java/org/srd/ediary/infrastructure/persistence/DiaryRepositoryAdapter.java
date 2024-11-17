2b58604470aa6e54ae44ceafb5e5852a4f588b0a
Aramis2424 <srd2424@yandex.ru>
2024-11-17 14:03:37 +0300

    add diary repository adapter

---
+++ src/main/java/org/srd/ediary/infrastructure/persistence/DiaryRepositoryAdapter.java
@@ -0,0 +1,49 @@
+package org.srd.ediary.infrastructure.persistence;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.srd.ediary.domain.model.Diary;
+import org.srd.ediary.domain.model.Owner;
+import org.srd.ediary.domain.repository.DiaryRepository;
+import org.srd.ediary.infrastructure.entity.DiaryEntity;
+import org.srd.ediary.infrastructure.mapper.DiaryEntityMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+interface SpringDiaryRepository extends CrudRepository<DiaryEntity, Long> {
+    List<DiaryEntity> findAllByOwner(Long id);
+}
+
+@Repository
+@RequiredArgsConstructor
+public class DiaryRepositoryAdapter implements DiaryRepository {
+    private final SpringDiaryRepository repo;
+    @Override
+    public Diary save(Diary model) {
+        DiaryEntity entity = DiaryEntityMapper.INSTANCE.modelToEntity(model);
+        DiaryEntity saved = repo.save(entity);
+        return DiaryEntityMapper.INSTANCE.entityToModel(saved);
+    }
+
+    @Override
+    public Optional<Diary> getByID(Long id) {
+        return repo.findById(id).map(DiaryEntityMapper.INSTANCE::entityToModel);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repo.deleteById(id);
+    }
+
+    @Override
+    public List<Diary> getAllByOwner(Long id) {
+        List<DiaryEntity> diaryEntities = repo.findAllByOwner(id);
+        List<Diary> diaries = new ArrayList<>(diaryEntities.size());
+        for (var diary : diaryEntities)
+            diaries.add(DiaryEntityMapper.INSTANCE.entityToModel(diary));
+        return diaries;
+    }
+}
