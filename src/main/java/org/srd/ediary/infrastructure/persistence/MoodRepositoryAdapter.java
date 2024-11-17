fa5838416c8ef40510db354bca207f723a2887c6
Aramis2424 <srd2424@yandex.ru>
2024-11-17 17:20:21 +0300

    add all adapters

---
+++ src/main/java/org/srd/ediary/infrastructure/persistence/MoodRepositoryAdapter.java
@@ -0,0 +1,49 @@
+package org.srd.ediary.infrastructure.persistence;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.srd.ediary.domain.model.Mood;
+import org.srd.ediary.domain.repository.MoodRepository;
+import org.srd.ediary.infrastructure.entity.MoodEntity;
+import org.srd.ediary.infrastructure.mapper.MoodEntityMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+interface SpringMoodRepository extends CrudRepository<MoodEntity, Long> {
+    List<MoodEntity> findAllByOwner(Long id);
+}
+
+@Repository
+@RequiredArgsConstructor
+public class MoodRepositoryAdapter implements MoodRepository {
+    private final SpringMoodRepository repo;
+
+    @Override
+    public Mood save(Mood model) {
+        MoodEntity entity = MoodEntityMapper.INSTANCE.modelToEntity(model);
+        MoodEntity saved = repo.save(entity);
+        return MoodEntityMapper.INSTANCE.entityToModel(saved);
+    }
+
+    @Override
+    public Optional<Mood> getByID(Long id) {
+        return repo.findById(id).map(MoodEntityMapper.INSTANCE::entityToModel);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repo.deleteById(id);
+    }
+
+    @Override
+    public List<Mood> getAllByOwner(Long id) {
+        List<MoodEntity> moodEntities = repo.findAllByOwner(id);
+        List<Mood> moods = new ArrayList<>(moodEntities.size());
+        for (var mood : moodEntities)
+            moods.add(MoodEntityMapper.INSTANCE.entityToModel(mood));
+        return moods;
+    }
+}
---
+++ src/main/java/org/srd/ediary/infrastructure/persistence/OwnerRepositoryAdapter.java
@@ -0,0 +1,51 @@
+package org.srd.ediary.infrastructure.persistence;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.srd.ediary.domain.model.Owner;
+import org.srd.ediary.domain.repository.OwnerRepository;
+import org.srd.ediary.infrastructure.entity.OwnerEntity;
+import org.srd.ediary.infrastructure.mapper.OwnerEntityMapper;
+
+import java.util.Optional;
+
+interface SpringOwnerRepository extends CrudRepository<OwnerEntity, Long> {
+    Optional<OwnerEntity> findByLoginAndPassword(String login, String password);
+    Optional<OwnerEntity> findByLogin(String login);
+}
+
+@Repository
+@RequiredArgsConstructor
+public class OwnerRepositoryAdapter implements OwnerRepository {
+    private final SpringOwnerRepository repo;
+
+    @Override
+    public Owner save(Owner model) {
+        OwnerEntity entity = OwnerEntityMapper.INSTANCE.modelToEntity(model);
+        OwnerEntity saved = repo.save(entity);
+        return OwnerEntityMapper.INSTANCE.entityToModel(saved);
+    }
+
+    @Override
+    public Optional<Owner> getByID(Long id) {
+        return repo.findById(id).map(OwnerEntityMapper.INSTANCE::entityToModel);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repo.deleteById(id);
+    }
+
+    @Override
+    public Optional<Owner> getByLoginAndPassword(String login, String password) {
+        return repo.findByLoginAndPassword(login, password)
+                .map(OwnerEntityMapper.INSTANCE::entityToModel);
+    }
+
+    @Override
+    public Optional<Owner> getByLogin(String login) {
+        return repo.findByLogin(login)
+                .map(OwnerEntityMapper.INSTANCE::entityToModel);
+    }
+}
