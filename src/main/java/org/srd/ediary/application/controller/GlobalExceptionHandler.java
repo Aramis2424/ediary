fc1d5c4ba4a9148f5b4df7576523a9df964f456c
Aramis2424 <srd2424@yandex.ru>
2024-11-18 22:56:49 +0300

    init global exception handler

---
+++ src/main/java/org/srd/ediary/application/controller/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package org.srd.ediary.application.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>("Internal Server Error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<String> handleNoHandlerFoundException(NoHandlerFoundException ex) {
+        return new ResponseEntity<>("The is not page, you are looking for...", HttpStatus.NOT_FOUND);
+    }
+}
