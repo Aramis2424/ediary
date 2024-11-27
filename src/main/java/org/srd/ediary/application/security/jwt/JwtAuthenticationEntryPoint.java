9cbc7f9fc27f2d85b251f46c963440626d28c4da
Aramis2424 <srd2424@yandex.ru>
2024-11-27 23:44:13 +0300

    modify error message

--- src/main/java/org/srd/ediary/application/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/org/srd/ediary/application/security/jwt/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,7 @@
 package org.srd.ediary.application.security.jwt;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,6 +11,8 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -19,10 +23,24 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
-        Exception exception = (Exception) request.getAttribute("exception");
+        Exception exception = (Exception) request.getAttribute("exception"); // TODO вырезать это из кода (в jftFilter тоже есть это)
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType(APPLICATION_JSON_VALUE);
-        response.getWriter().write(authException.getMessage());
-        log.error("Authentication Exception: {} ", exception, authException);
+        response.getWriter().write(getErrorJson(authException.getMessage()));
+
+        log.error("Authentication Exception: {} ", authException.getMessage(), authException);
+    }
+
+    private String getErrorJson(String error) {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        errorDetails.put("error", "Unauthorized");
+        errorDetails.put("message", error);
+        try {
+            return mapper.writeValueAsString(errorDetails);
+        } catch (JsonProcessingException ex) {
+            return "{\"error\": \"Unknown authorization error\"}";
+        }
     }
 }
