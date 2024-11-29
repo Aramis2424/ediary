ba3273c469fa32823f7a4ddb08195b6e2d67a21f
Aramis2424 <srd2424@yandex.ru>
2024-11-29 22:53:59 +0300

    add new tests for diary service

--- src/test/java/org/srd/ediary/application/service/DiaryServiceTest.java
+++ src/test/java/org/srd/ediary/application/service/DiaryServiceTest.java
@@ -8,7 +8,11 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.srd.ediary.application.dto.DiaryCreateDTO;
 import org.srd.ediary.application.dto.DiaryInfoDTO;
+import org.srd.ediary.application.dto.DiaryUpdateDTO;
+import org.srd.ediary.application.exception.DiaryNotFoundException;
+import org.srd.ediary.application.exception.OwnerNotFoundException;
 import org.srd.ediary.domain.model.Diary;
+import org.srd.ediary.domain.model.Entry;
 import org.srd.ediary.domain.model.Owner;
 import org.srd.ediary.domain.repository.DiaryRepository;
 import org.srd.ediary.domain.repository.EntryRepository;
@@ -19,7 +23,9 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.postgresql.hostchooser.HostRequirement.any;
 
 @ExtendWith(MockitoExtension.class)
 class DiaryServiceTest {
@@ -32,11 +38,13 @@ class DiaryServiceTest {
     @InjectMocks
     private DiaryService service;
 
+    private final LocalDate birthdate = LocalDate.of(2000, 1, 1);
+    private final Owner owner = new Owner("Ivan", birthdate, "example", "abc123");
+
     @Test
-    void testGetDiary() {
+    void testGetDiaryExisting() {
         Long id = 1L;
-        Owner owner = new Owner("Ivan", LocalDate.of(2000, 1, 1),
-                "example", "abc123");
+        Owner owner = new Owner("Ivan", birthdate, "example", "abc123");
         Diary diary = new Diary(owner, "d1", "about");
         DiaryInfoDTO expected = new DiaryInfoDTO(null,"d1", "about",
                 0, LocalDate.now());
@@ -48,10 +56,17 @@ class DiaryServiceTest {
     }
 
     @Test
+    void testGetDiaryNonExisting() {
+        Long id = 1L;
+        when(diaryRepo.getByID(id)).thenReturn(Optional.empty());
+
+        assertThrows(DiaryNotFoundException.class, () -> service.getDiary(1L));
+    }
+
+    @Test
     void testGetOwnerDiaries() {
         Long ownerID = 1L;
-        Owner owner = new Owner("Ivan", LocalDate.of(2000, 1, 1),
-                "example", "abc123");
+        Owner owner = new Owner("Ivan", birthdate, "example", "abc123");
         Diary d1 = new Diary(owner, "d1", "of1");
         Diary d2 = new Diary(owner, "d2", "of2");
         Diary d3 = new Diary(owner, "d3", "of3");
@@ -68,17 +83,106 @@ class DiaryServiceTest {
     }
 
     @Test
-    void testCreate() {
+    void testGetOwnerDiariesEmpty() {
+        Long ownerID = 1L;
+        List<Diary> diaries = List.of();
+        List<DiaryInfoDTO> expected = List.of();
+        when(diaryRepo.getAllByOwner(ownerID)).thenReturn(diaries);
+
+        List<DiaryInfoDTO> actual = service.getOwnerDiaries(ownerID);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCreateExistingOwner() {
         DiaryCreateDTO input = new DiaryCreateDTO(1L, "d1", "of1");
         DiaryInfoDTO expected = new DiaryInfoDTO(null,"d1", "of1", 0, LocalDate.now());
-        Owner owner = new Owner("Ivan", LocalDate.of(2000, 1, 1),
-                "example", "abc123");
+        Owner owner = new Owner("Ivan", birthdate, "example", "abc123");
         Diary diary = new Diary(owner, "d1", "of1");
         when(ownerRepo.getByID(1L)).thenReturn(Optional.of(owner));
-        when(diaryRepo.save(Mockito.any(Diary.class))).thenReturn(diary);
+        when(diaryRepo.save(any(Diary.class))).thenReturn(diary);
 
         DiaryInfoDTO actual = service.create(input);
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCreateNonExistingOwner() {
+        DiaryCreateDTO input = new DiaryCreateDTO(1L, "d1", "of1");
+        when(ownerRepo.getByID(1L)).thenReturn(Optional.empty());
+
+        assertThrows(OwnerNotFoundException.class, () -> service.create(input));
+
+        verify(diaryRepo, never()).save(any(Diary.class));
+    }
+
+    @Test
+    void testUpdateExistingDiary() {
+        Long id = 1L;
+        Owner owner = new Owner("Ivan", birthdate, "example", "abc123"); // TODO вынести в поле класса
+        Diary diary = new Diary(owner, "d1", "about");
+        DiaryUpdateDTO updateDiary = new DiaryUpdateDTO("d2", "of");
+        Diary diaryUpdated = new Diary(owner, "d2", "of");
+        DiaryInfoDTO expected = new DiaryInfoDTO(null,"d2", "of", 0, LocalDate.now());
+        when(diaryRepo.getByID(id)).thenReturn(Optional.of(diary));
+        when(diaryRepo.save(any(Diary.class))).thenReturn(diaryUpdated);
+
+        DiaryInfoDTO actual = service.update(id, updateDiary);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testUpdateNonExistingDiary() {
+        Long id = 1L;
+        DiaryUpdateDTO newDiary = new DiaryUpdateDTO("d2", "of");
+        when(diaryRepo.getByID(id)).thenReturn(Optional.empty());
+
+        assertThrows(DiaryNotFoundException.class, () -> service.update(id, newDiary));
+    }
+
+    @Test
+    void testRemoveExistingWithEntries() {
+        Long id = 1L;
+        Diary diary = new Diary(owner, "d1", "about");
+        Entry e1 = new Entry(diary, "e1", "about e1");
+        Entry e2 = new Entry(diary, "e2", "about e2");
+        List<Entry> entries = List.of(e1, e2);
+        when(entryRepo.getAllByDiary(id)).thenReturn(entries);
+        doNothing().when(entryRepo).delete(any());
+        doNothing().when(diaryRepo).delete(id);
+
+        service.remove(id);
+
+        verify(entryRepo, times(2)).delete(any());
+        verify(diaryRepo, times(1)).delete(id);
+    }
+
+    @Test
+    void testRemoveExistingWithoutEntries() {
+        Long id = 1L;
+        List<Entry> entries = List.of();
+        when(entryRepo.getAllByDiary(id)).thenReturn(entries);
+        doNothing().when(diaryRepo).delete(id);
+
+        service.remove(id);
+
+        verify(entryRepo, never()).delete(any());
+        verify(diaryRepo, times(1)).delete(id);
+    }
+
+    @Test
+    void testRemoveNonExisting() {
+        Long id = 1L;
+        List<Entry> entries = List.of();
+        when(entryRepo.getAllByDiary(id)).thenReturn(entries);
+        doNothing().when(diaryRepo).delete(id);
+
+        service.remove(id);
+
+        verify(entryRepo, never()).delete(any());
+        verify(diaryRepo, times(1)).delete(id);
     }
 }
