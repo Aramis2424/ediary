6eb7823f845d04375bda305936b127169dec6f2a
Aramis2424 <srd2424@yandex.ru>
2024-12-16 19:42:21 +0300

    impl entry request

--- frontend/ediary_cli/src/main/kotlin/org/srd/ediary_cli/controller/EntryController.kt
+++ frontend/ediary_cli/src/main/kotlin/org/srd/ediary_cli/controller/EntryController.kt
@@ -3,7 +3,9 @@ package org.srd.ediary_cli.controller
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import org.srd.ediary_cli.model.EntryCreateDTO
 import org.srd.ediary_cli.model.EntryInfoDTO
+import org.srd.ediary_cli.model.EntryUpdateDTO
 import org.srd.ediary_cli.service.HttpService
 import org.srd.ediary_cli.util.IOUtil
 import org.srd.ediary_cli.util.LocalStorage
@@ -30,6 +32,41 @@ class EntryController(diaryId: Long) {
 
             when (ioUtil.input()) {
                 "0" -> return
+                "1" -> {
+                    val entry = executor.getEntryRequest()
+                    if (entry == null) {
+                        println("Что-то пошло не так")
+                    } else {
+                        println("""id = ${entry.id}
+                            |заглавие = ${entry.title}
+                            |содержание = ${entry.content}
+                        """.trimMargin())
+                        println()
+                    }
+                }
+                "2" -> {
+                    val entry = executor.createEntryRequest()
+                    if (entry == null) {
+                        println("Что-то пошло не так...")
+                    } else {
+                        println("Запись успешно создана")
+                    }
+                }
+                "3" -> {
+                    val entry = executor.updateEntryRequest()
+                    if (entry == null) {
+                        println("Что-то пошло не так...")
+                    } else {
+                        println("Запись успешно изменена")
+                    }
+                }
+                "4" -> {
+                    if (executor.deleteEntryRequest()) {
+                        println("Запись успешно удалена")
+                    } else {
+                        println("Что-то пошло не так")
+                    }
+                }
                 else -> ioUtil.outputInvalidChoice()
             }
         }
@@ -51,8 +88,91 @@ private class EntryExec(val diaryId: Long) {
                 append(HttpHeaders.Authorization, "Bearer ${LocalStorage.token}")
             }
         }
-        println(diaryId)
-        println(res)
+        return try {
+            res.body()
+        } catch (ex : Exception) {
+            null
+        }
+    }
+
+    suspend fun createEntryRequest(): EntryInfoDTO? {
+        val title = ioUtil.inputString("Введите заглавие записи: ")
+        val content = ioUtil.inputString("Введите содержание записи: ")
+
+        val entryCreateDTO = EntryCreateDTO(diaryId, title, content)
+
+        return sendEntryRequest(entryCreateDTO)
+    }
+
+    private suspend fun sendEntryRequest(entryCreateDTO: EntryCreateDTO): EntryInfoDTO? {
+        val res = client.post("${HttpService.BASE_URL}/entries") {
+            contentType(ContentType.Application.Json)
+            setBody(entryCreateDTO)
+            headers {
+                append(HttpHeaders.Authorization, "Bearer ${LocalStorage.token}")
+            }
+        }
+        return try {
+            res.body()
+        } catch (ex : Exception) {
+            null
+        }
+    }
+
+    suspend fun getEntryRequest(): EntryInfoDTO? {
+        val entryId = ioUtil.inputNumber("Укажите id записи, которую хотите открыть: ")
+        return sendGetEntryRequest(entryId)
+    }
+
+    private suspend fun sendGetEntryRequest(entryId: Int): EntryInfoDTO? {
+        val res = client.get("${HttpService.BASE_URL}/entries/$entryId") {
+            contentType(ContentType.Application.Json)
+            headers {
+                append(HttpHeaders.Authorization, "Bearer ${LocalStorage.token}")
+            }
+        }
+        return try {
+            res.body()
+        } catch (ex : Exception) {
+            null
+        }
+    }
+
+    suspend fun deleteEntryRequest(): Boolean {
+        val entryId = ioUtil.inputNumber("Укажите id записи, которую хотите удалить: ")
+        return sendDeleteDiaryRequest(entryId)
+    }
+
+    private suspend fun sendDeleteDiaryRequest(entryId: Int): Boolean {
+        val res = client.delete("${HttpService.BASE_URL}/entries/$entryId") {
+            contentType(ContentType.Application.Json)
+            headers {
+                append(HttpHeaders.Authorization, "Bearer ${LocalStorage.token}")
+            }
+        }
+        if (res.status.value != 200) {
+            return false
+        }
+        return true
+    }
+
+    suspend fun updateEntryRequest(): EntryInfoDTO? {
+        val entryId = ioUtil.inputNumber("Укажите id записи, которую хотите изменить: ")
+        val title = ioUtil.inputString("Введите заглавие записи: ")
+        val content = ioUtil.inputString("Введите содержание записи: ")
+
+        val entryUpdateDto = EntryUpdateDTO(title, content)
+        return sendUpdateDiaryRequest(entryId, entryUpdateDto)
+    }
+
+    private suspend fun sendUpdateDiaryRequest(entryId: Int, entryUpdateDto: EntryUpdateDTO): EntryInfoDTO? {
+        val res = client.put("${HttpService.BASE_URL}/entries/$entryId") {
+            contentType(ContentType.Application.Json)
+            setBody(entryUpdateDto)
+            headers {
+                append(HttpHeaders.Authorization, "Bearer ${LocalStorage.token}")
+            }
+        }
         return try {
             res.body()
         } catch (ex : Exception) {
