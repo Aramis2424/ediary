bd03c1a57811644c47e4b894ea8e709d1c9ee776
Aramis2424 <srd2424@yandex.ru>
2024-12-16 17:28:26 +0300

    impl deletion and creation diaries

--- frontend/ediary_cli/src/main/kotlin/org/srd/ediary_cli/controller/DiaryController.kt
+++ frontend/ediary_cli/src/main/kotlin/org/srd/ediary_cli/controller/DiaryController.kt
@@ -3,6 +3,7 @@ package org.srd.ediary_cli.controller
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import org.srd.ediary_cli.model.DiaryCreateDTO
 import org.srd.ediary_cli.model.DiaryInfoDTO
 import org.srd.ediary_cli.service.HttpService
 import org.srd.ediary_cli.util.IOUtil
@@ -16,7 +17,7 @@ class DiaryController {
 
     suspend fun start() {
         val diaries = executor.getDiariesRequest()
-        if (diaries == null) {
+        if (diaries.isNullOrEmpty()) {
             println("У Вас пока нет дневников ...")
         } else {
             println("Ваши дневники:")
@@ -32,8 +33,19 @@ class DiaryController {
                 "1" -> {
                 }
                 "2" -> {
+                    val diary = executor.createDiaryRequest()
+                    if (diary == null) {
+                        println("Что-то пошло не так...")
+                    } else {
+                        println("Дневник успешно создан")
+                    }
                 }
                 "3" -> {
+                    if (executor.deleteDiaryRequest()) {
+                        println("Дневник успешно удален")
+                    } else {
+                        println("Что-то пошло не так")
+                    }
                 }
                 else -> ioUtil.outputInvalidChoice()
             }
@@ -47,10 +59,10 @@ private class DiaryExec {
 
     suspend fun getDiariesRequest(): List<DiaryInfoDTO>? {
         val ownerId = LocalStorage.currentOwner?.id
-        return sendGetdiariesRequest(ownerId)
+        return sendGetDiariesRequest(ownerId)
     }
 
-    private suspend fun sendGetdiariesRequest(ownerId: Long?): List<DiaryInfoDTO>? {
+    private suspend fun sendGetDiariesRequest(ownerId: Long?): List<DiaryInfoDTO>? {
         val res = client.get("${HttpService.BASE_URL}/diaries/owner/$ownerId") {
             contentType(ContentType.Application.Json)
             headers {
@@ -62,5 +74,48 @@ private class DiaryExec {
         } catch (ex : Exception) {
             null
         }
+    }
+
+    suspend fun createDiaryRequest(): DiaryInfoDTO? {
+        val ownerId = LocalStorage.currentOwner?.id
+        val title = ioUtil.inputString("Введите название дневника: ")
+        val description = ioUtil.inputString("Введите краткое описание дневника: ")
+
+        val diaryCreateDto = DiaryCreateDTO(ownerId!!, title, description)
+
+        return sendCreateDiaryRequest(diaryCreateDto)
+    }
+
+    private suspend fun sendCreateDiaryRequest(diaryCreateDTO: DiaryCreateDTO): DiaryInfoDTO? {
+        val res = client.post("${HttpService.BASE_URL}/diaries") {
+            contentType(ContentType.Application.Json)
+            setBody(diaryCreateDTO)
+            headers {
+                append(HttpHeaders.Authorization, "Bearer ${LocalStorage.token}")
+            }
+        }
+        return try {
+            res.body()
+        } catch (ex : Exception) {
+            null
+        }
+    }
+
+    suspend fun deleteDiaryRequest(): Boolean {
+        val diaryId = ioUtil.inputNumber("Укажите id дневника, который хотите удалить: ")
+        return sendDeleteDiaryRequest(diaryId)
+    }
+
+    private suspend fun sendDeleteDiaryRequest(diaryId: Int): Boolean {
+        val res = client.delete("${HttpService.BASE_URL}/diaries/$diaryId") {
+            contentType(ContentType.Application.Json)
+            headers {
+                append(HttpHeaders.Authorization, "Bearer ${LocalStorage.token}")
+            }
+        }
+        if (res.status.value != 200) {
+            return false
+        }
+        return true
     }
 }
