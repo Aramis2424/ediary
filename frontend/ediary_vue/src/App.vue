941343f525b77da03f22a590f18b53943b0c404e
Aramis2424 <srd2424@yandex.ru>
2025-05-26 13:31:10 +0300

    impl home page

--- frontend/ediary_vue/src/App.vue
+++ frontend/ediary_vue/src/App.vue
@@ -1,22 +1,27 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+   <div class="h-screen flex flex-col items-center justify-between p-4 relative bg-lemon">
+    <h1 class="font-czizh text-white bg-[#B4B4B4] bg-opacity-10 text-6xl absolute top-4 left-1/2 -translate-x-1/2">
+      Ваши мысли!
+    </h1>
+    <div class="flex flex-1 w-full items-center justify-between">
+      <button class="sideBtnL"> О себе </button>
+
+      <div class="flex flex-col items-center justify-center gap-4 h-full w-full max-w-md">
+        <button class="w-[30vw] h-16 baseBtn text-black text-2xl md:text-3xl lg:text-4xl font-czizh">
+          Личный дневник
+        </button>
+        <button class="w-[30vw] h-16 baseBtn text-black text-2xl md:text-3xl lg:text-4xl font-czizh">
+          Настроения
+        </button>
+      </div>
+
+      <button class="sideBtnR" style="letter-spacing: 2px;"> Настройки </button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- frontend/ediary_vue/src/main.ts
+++ frontend/ediary_vue/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './assets/main.css'
-import './style.css'
+import './assets/fonts.css'
+import './assets/backgrounds.css';
 import App from './App.vue'
 
 createApp(App).mount('#app')
