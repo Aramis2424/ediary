3b5402484b740693eaf04ff4ab7b6ab5c1e81bf8
Aramis2424 <srd2424@yande.ru>
2025-05-30 15:08:41 +0300

    add md editor

---
+++ frontend/ediary_vue/src/components/MarkdownEditor.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <!-- <div class="h-screen flex">
+    <div class="w-[90vw] h-full mx-auto">
+      <MarkdownEditor class="w-full h-full" />
+    </div>
+  </div> -->
+  <v-md-editor v-model="content" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import VMdEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+import ruRU from '@kangc/v-md-editor/lib/lang/ru-RU';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+
+import Prism from 'prismjs';
+
+VMdEditor.use(githubTheme, { Prism });
+VMdEditor.lang.use('ru-RU', ruRU)
+
+const content = ref<string>('Ваши *мысли* ...');
+</script>
