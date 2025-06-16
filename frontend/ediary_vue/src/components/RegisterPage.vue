ad073b107cd2753e151cdda2c31eb87425f41777
Aramis2424 <srd2424@yandex.ru>
2025-06-16 17:45:45 +0300

    fix bug in y-spacing

--- frontend/ediary_vue/src/components/RegisterPage.vue
+++ frontend/ediary_vue/src/components/RegisterPage.vue
@@ -1,51 +1,53 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-100 bg-lemon">
     <div class="w-full max-w-md bg-white p-8 rounded-2xl text-[#5A3A2B] font-semibold shadow opacity-95">
-      <h2 class="text-3xl text-center mb-6"> Регистрация </h2>
-      <form @submit.prevent="handleRegister" class="space-y-4">
-        <div class="space-y-4">
-            <p class="text-xl text-[#5A3A2B] -mb-3"> Имя </p>
-            <input
-            v-model="name"
-            type="text"
-            placeholder="Введите Ваше имя"
-            class="w-full p-3 rounded-2xl border-[#F4A261] border-2 focus:outline-none focus:ring-1 focus:ring-[#e09455]"
-            />
-        </div>
-        <div class="space-y-4">
-            <p class="text-xl text-[#5A3A2B] -mb-3"> Логин </p>
-            <input
-            v-model="login"
-            type="text"
-            placeholder="Введите Ваш логин"
-            class="w-full p-3 rounded-2xl border-[#F4A261] border-2 focus:outline-none focus:ring-1 focus:ring-[#e09455]"
-            />
-        </div>
-        <div class="space-y-4">
-            <p class="text-xl text-[#5A3A2B] -mb-3"> Пароль </p>
-            <input
-            v-model="password"
-            type="password"
-            placeholder="Введите Ваш пароль"
-            class="w-full p-3 rounded-2xl border-[#F4A261] border-2 focus:outline-none focus:ring-1 focus:ring-[#e09455]"
-            />
-        </div>
-        <div class="space-y-4">
-            <p class="text-xl text-[#5A3A2B] -mb-3"> Повторите пароль </p>
-            <input
-            v-model="confirmPassword"
-            type="password"
-            placeholder="Повторите Ваш пароль"
-            class="w-full p-3 rounded-2xl border-[#F4A261] border-2 focus:outline-none focus:ring-1 focus:ring-[#e09455]"
-            />
-        </div>
-        <div class="space-y-4">
-            <p class="text-xl text-[#5A3A2B] -mb-3"> Дата рождения </p>
-            <input
-            v-model="confirmPassword"
-            type="date"
-            class="w-full p-3 rounded-2xl border-[#F4A261] border-2 focus:outline-none focus:ring-1 focus:ring-[#e09455]"
-            />
+      <h2 class="text-3xl text-center mb-3"> Регистрация </h2>
+      <form @submit.prevent="handleRegister" class="space-y-8">
+        <div class="space-y-3">
+            <div class="space-y-4">
+                <p class="text-xl text-[#5A3A2B] -mb-3"> Имя </p>
+                <input
+                v-model="name"
+                type="text"
+                placeholder="Введите Ваше имя"
+                class="w-full p-3 rounded-2xl border-[#F4A261] border-2 focus:outline-none focus:ring-1 focus:ring-[#e09455]"
+                />
+            </div>
+            <div class="space-y-4">
+                <p class="text-xl text-[#5A3A2B] -mb-3"> Логин </p>
+                <input
+                v-model="login"
+                type="text"
+                placeholder="Введите Ваш логин"
+                class="w-full p-3 rounded-2xl border-[#F4A261] border-2 focus:outline-none focus:ring-1 focus:ring-[#e09455]"
+                />
+            </div>
+            <div class="space-y-4">
+                <p class="text-xl text-[#5A3A2B] -mb-3"> Пароль </p>
+                <input
+                v-model="password"
+                type="password"
+                placeholder="Введите Ваш пароль"
+                class="w-full p-3 rounded-2xl border-[#F4A261] border-2 focus:outline-none focus:ring-1 focus:ring-[#e09455]"
+                />
+            </div>
+            <div class="space-y-4">
+                <p class="text-xl text-[#5A3A2B] -mb-3"> Повторите пароль </p>
+                <input
+                v-model="confirmPassword"
+                type="password"
+                placeholder="Повторите Ваш пароль"
+                class="w-full p-3 rounded-2xl border-[#F4A261] border-2 focus:outline-none focus:ring-1 focus:ring-[#e09455]"
+                />
+            </div>
+            <div class="space-y-4">
+                <p class="text-xl text-[#5A3A2B] -mb-3"> Дата рождения </p>
+                <input
+                v-model="confirmPassword"
+                type="date"
+                class="w-full p-3 rounded-2xl border-[#F4A261] border-2 focus:outline-none focus:ring-1 focus:ring-[#e09455]"
+                />
+            </div>
         </div>
         <button
           type="submit"
