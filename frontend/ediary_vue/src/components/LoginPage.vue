301de10936607d9738c39b0fe112bc784c5a76fc
Aramis2424 <srd2424@yandex.ru>
2025-06-16 17:46:01 +0300

    impl login template

---
+++ frontend/ediary_vue/src/components/LoginPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100 bg-lemon">
+    <div class="w-full max-w-md bg-white p-8 rounded-2xl text-[#5A3A2B] font-semibold shadow opacity-95">
+      <h2 class="text-3xl text-center mb-3"> Вход </h2>
+      <form @submit.prevent="handleRegister" class="space-y-8">
+        <div class="space-y-3">
+            <div class="space-y-4">
+                <p class="text-xl text-[#5A3A2B] -mb-3"> Логин </p>
+                <input
+                v-model="login"
+                type="text"
+                placeholder="Введите Ваш логин"
+                class="w-full p-3 rounded-2xl border-[#F4A261] border-2 focus:outline-none focus:ring-1 focus:ring-[#e09455]"
+                />
+            </div>
+            <div class="space-y-4">
+                <p class="text-xl text-[#5A3A2B] -mb-3"> Пароль </p>
+                <input
+                v-model="password"
+                type="password"
+                placeholder="Введите Ваш пароль"
+                class="w-full p-3 rounded-2xl border-[#F4A261] border-2 focus:outline-none focus:ring-1 focus:ring-[#e09455]"
+                />
+            </div>
+        </div>
+        <button
+          type="submit"
+          class="baseBtn p-3 w-full text-xl"
+        >
+          Войти
+        </button>
+      </form>
+      <p class="text-base text-center">
+        Нет аккаунта?
+        <router-link to="/login" class="text-[#E67E22] font-bold hover:underline"> Регистрация </router-link>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const login = ref('');
+const password = ref('');
+
+function handleRegister() {
+  console.log('Регистрация:', login.value, password.value);
+  // TODO: логика регистрации
+}
+</script>
