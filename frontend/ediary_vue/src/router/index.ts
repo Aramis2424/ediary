9f714b7ca14e5e9bfcf25157c90dfda076b6e2ff
Aramis2424 <srd2424@yandex.ru>
2025-06-17 16:40:07 +0300

    init paths

--- frontend/ediary_vue/src/router/index.ts
+++ frontend/ediary_vue/src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginPage from '@/components/LoginPage.vue'
-import RegisterPage from '@/components/RegisterPage.vue'
-import Home from '@/components/Home.vue'
+import LoginPage from '../components/LoginPage.vue'
+import RegisterPage from '../components/RegisterPage.vue'
+import Home from '../components/Home.vue'
+import MoodGraph from '../components/MoodGraph.vue'
+import EntriesMenu from '../components/EntriesMenu.vue'
+import MarkdownEditor from '../components/MarkdownEditor.vue'
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: LoginPage },
   { path: '/register', component: RegisterPage },
   { path: '/home', component: Home },
+  { path: '/graph', component: MoodGraph },
+  { path: '/menu', component: EntriesMenu },
+  { path: '/entry/:id', component: MarkdownEditor },
 ]
 
 const router = createRouter({
