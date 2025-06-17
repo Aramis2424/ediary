e06c7178f85f67ebf596a4e26603848fd6758e90
Aramis2424 <srd2424@yandex.ru>
2025-06-17 19:15:59 +0300

    fix style of entries menu

--- frontend/ediary_vue/src/components/EntriesMenu.vue
+++ frontend/ediary_vue/src/components/EntriesMenu.vue
@@ -3,9 +3,7 @@ import EntryCard from './EntryCard.vue';
 </script>
 
 <template>
-<!-- <div class="h-screen flex justify-center items-center">
-    <EntriesMenu />
-</div> -->
+<div class="h-screen flex justify-center items-center bg-fire">
   <div class="h-[90vh] w-full max-w-5xl flex flex-wrap gap-5 justify-center content-start p-4 overflow-y-scroll hide-scrollbar">
     <EntryCard title="День трех встречь" date="2020-01-05" :mood="7" :work="9"/>
     <EntryCard title="Много разговоров" date="2020-01-06" :mood="5" :work="8"/>
@@ -14,6 +12,7 @@ import EntryCard from './EntryCard.vue';
     <EntryCard title="17 часов ..." date="2020-01-09" :mood="9" :work="2"/>
     <EntryCard title="Работал за двоих" date="2020-01-10" :mood="1" :work="10"/>
   </div>
+</div>
 </template>
 
 <style scoped>
