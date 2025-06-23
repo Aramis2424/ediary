83acdb9ce76487ecd7270e60ffdd7c8c4454af58
Aramis2424 <srd2424@yandex.ru>
2025-06-23 13:59:59 +0300

    add template button for creating new entry

--- frontend/ediary_vue/src/components/EntriesMenu.vue
+++ frontend/ediary_vue/src/components/EntriesMenu.vue
@@ -4,6 +4,7 @@ import { ref, onMounted } from 'vue'
 import { api } from '@/api/axios';
 import { useOwnerStore } from '../stores/owner';
 import EntryCard from '@/components/EntryCard.vue';
+import NewEntryCard from './NewEntryCard.vue';
 import type { EntryCard as EntryCardDto } from '@/types/EntryCard';
 
 const router = useRouter();
@@ -27,6 +28,7 @@ const gotoHome = () => {router.push('/home')}
 <div class="h-screen w-full flex justify-between items-center px-2 bg-fire">
   <button class="sideBtnL" @click="gotoHome"> Назад </button>
   <div class="h-[90vh] w-full max-w-5xl flex flex-wrap gap-5 justify-center content-start p-4 overflow-y-scroll hide-scrollbar">
+    <NewEntryCard />
     <EntryCard v-for="entry in entries" :key="entry.entryId" :entry="entry" @clicked="gotoEntry(entry.entryId)" />
   </div>
   <button class="sideBtnR"> Поиск записей </button>
---
+++ frontend/ediary_vue/src/components/NewEntryCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineEmits(['clicked'])
+</script>
+
+<template>
+  <div @click="$emit('clicked')" class="flex justify-center items-center baseBtn w-1/5 h-1/5 min-w-72 min-h-36">
+    <div class="relative w-[3vw] h-[3vw] rounded-full bg-[#F4A261] opacity-90 ">
+      <div class="absolute top-1/2 left-1/2 w-[10%] h-[70%] bg-white -translate-x-1/2 -translate-y-1/2 rounded"></div>
+      <div class="absolute top-1/2 left-1/2 w-[70%] h-[10%] bg-white -translate-x-1/2 -translate-y-1/2 rounded"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
