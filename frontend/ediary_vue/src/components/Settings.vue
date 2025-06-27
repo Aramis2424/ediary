210598014f95ba4016bf5e81f755807cf8f8e465
Aramis2424 <srd2424@yandex.ru>
2025-06-27 18:37:19 +0300

    fix size bug in settings menu

--- frontend/ediary_vue/src/components/Settings.vue
+++ frontend/ediary_vue/src/components/Settings.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="baseBacking">
-    <div class="bg-white rounded-xl p-2 w-1/6 h-1/6 shadow-xl relative flex flex-col justify-evenly content-center items-center">
+    <div class="bg-white rounded-xl p-2 w-1/6 min-h-1/6 shadow-xl relative flex flex-col justify-evenly content-center items-center gap-2">
         <h2 class="text-xl font-semibold mb-4">Настройки</h2>
-        <a :href="mailtoLink" class="baseBtn w-1/2 text-center">
+        <a :href="mailtoLink" class="baseBtn w-5/6 text-center">
             Сообщить о баге
         </a>
-        <button @click="logout" class="baseBtn w-1/2">
+        <button @click="logout" class="baseBtn w-5/6">
             Выйти
         </button>
         <button @click="$emit('clicked')" class="absolute top-2 right-2 text-gray-500 hover:text-black">
