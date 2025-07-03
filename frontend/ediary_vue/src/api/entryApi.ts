a63d68beca71c8365e25ad0f1cba185ea91ad6a6
Aramis2424 <srd2424@yandex.ru>
2025-07-03 12:59:00 +0300

    impl put & delete methods

--- frontend/ediary_vue/src/api/entryApi.ts
+++ frontend/ediary_vue/src/api/entryApi.ts
@@ -1,6 +1,6 @@
 import { api } from './axios.ts'
 import type { AxiosResponse } from 'axios';
-import type { EntryInfoDTO, EntryCreateDTO } from '@/types/Entry.ts';
+import type { EntryInfoDTO, EntryCreateDTO, EntryUpdateDTO } from '@/types/Entry.ts';
 
 export const getEntry = async (entryId: number): Promise<AxiosResponse<EntryInfoDTO>> => {
   const res: AxiosResponse<EntryInfoDTO> = await api.get(`/entries/${entryId}`);
@@ -10,5 +10,16 @@ export const getEntry = async (entryId: number): Promise<AxiosResponse<EntryInfo
 export const postEntry = async (newEntry: EntryCreateDTO): Promise<AxiosResponse<EntryInfoDTO>> => {
   const res = await api.post<EntryInfoDTO, AxiosResponse<EntryInfoDTO>, EntryCreateDTO>(
                 '/entries/', newEntry)
+  return res;
+};
+
+export const putEntry = async (entryId: number, updatedEntry: EntryUpdateDTO): Promise<AxiosResponse<EntryInfoDTO>> => {
+  const res = await api.put<EntryInfoDTO, AxiosResponse<EntryInfoDTO>, EntryUpdateDTO>(
+                `/entries/${entryId}`, updatedEntry)
+  return res;
+};
+
+export const deleteEntry = async (entryId: number): Promise<AxiosResponse<void>> => {
+  const res: AxiosResponse<void> = await api.delete(`/entries/${entryId}`);
   return res;
 };
--- frontend/ediary_vue/src/services/entryService.ts
+++ frontend/ediary_vue/src/services/entryService.ts
@@ -1,5 +1,5 @@
-import { postEntry, getEntry } from '@/api/entryApi';
-import type { EntryInfoDTO, EntryCreateDTO } from '@/types/Entry';
+import { postEntry, getEntry, deleteEntry, putEntry } from '@/api/entryApi';
+import type { EntryInfoDTO, EntryCreateDTO, EntryUpdateDTO } from '@/types/Entry';
 
 export const fetchEntry = async (entryId: number): Promise<EntryInfoDTO> => {
     try {
@@ -14,7 +14,7 @@ export const fetchEntry = async (entryId: number): Promise<EntryInfoDTO> => {
         } else if (error.response?.status === 403) {
             throw new Error("Access denied");
         } else {
-        throw error;
+            throw error;
         }
     }
 };
@@ -31,6 +31,35 @@ export const createEntry = async (diaryId: number): Promise<EntryInfoDTO> => {
             throw new Error('Cannot create entry');
         }
         return res.data;
+    } catch (error: any) {
+        throw error;
+    }
+};
+
+export const updateEntry = async (entryId: number, updatedEntry: EntryUpdateDTO): Promise<EntryInfoDTO> => {
+    try {
+        const res = await putEntry(entryId, updatedEntry);
+        if (!res.data || res.status !== 200) {
+            throw new Error('Cannot update entry');
+        }
+        return res.data;
+    } catch (error: any) {
+        if (error.response?.status === 404) {
+            throw new Error("Entry not found");
+        } else if (error.response?.status === 403) {
+            throw new Error("Access denied");
+        } else {
+            throw error;
+        }
+    }
+};
+
+export const removeEntry = async (entryId: number): Promise<void> => {
+    try {
+        const res = await deleteEntry(entryId);
+        if (res.status !== 204) {
+            throw new Error('Cannot remove entry');
+        }
     } catch (error: any) {
         throw error;
     }
