97a6894b724c3193300343e3a57e945bcfd07390
Aramis2424 <srd2424@yandex.ru>
2025-07-07 19:13:28 +0300

    impl service method for getting entry cards

--- src/main/java/org/srd/ediary/application/service/EntryCardService.java
+++ src/main/java/org/srd/ediary/application/service/EntryCardService.java
@@ -5,16 +5,40 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.srd.ediary.application.dto.EntryCardDTO;
+import org.srd.ediary.application.dto.EntryInfoDTO;
+import org.srd.ediary.application.dto.MoodInfoDTO;
+import org.srd.ediary.application.security.utils.AuthHelper;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class EntryCardService {
+    private final AuthHelper authHelper;
+    private final EntryService entryService;
+    private final MoodService moodService;
+
     @PreAuthorize("@entryAccess.isDiaryBelongsOwner(#diaryId, authentication.principal.id)")
     public List<EntryCardDTO> getEntryCards(Long diaryId) {
-        return List.of(new EntryCardDTO(1L, 2L, "Title01", 8, 7, LocalDate.now()));
+        Long ownerId = authHelper.getCurrentUserId();
+
+        List<EntryInfoDTO> diaryEntries = entryService.getAllEntriesByDiary(diaryId);
+        List<MoodInfoDTO> ownerMoods = moodService.getMoodsByOwner(ownerId);
+        List<EntryCardDTO> cards = new ArrayList<>(diaryEntries.size());
+
+        for (var entry: diaryEntries) {
+            Optional<MoodInfoDTO> mood = ownerMoods.stream().filter(
+                        it -> it.createdAt().isEqual(entry.createdDate()))
+                    .findFirst();
+            EntryCardDTO card = new EntryCardDTO(entry.id(), diaryId, entry.title(),
+                    mood.map(MoodInfoDTO::scoreMood).orElse(-1),
+                    mood.map(MoodInfoDTO::scoreProductivity).orElse(-1),
+                    entry.createdDate());
+            cards.add(card);
+        }
+        return cards;
     }
 }
