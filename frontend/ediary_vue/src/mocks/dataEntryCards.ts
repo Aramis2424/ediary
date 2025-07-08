5537862e88c3cca8a6bbe761b11015737274ec06
Aramis2424 <srd2424@yandex.ru>
2025-07-08 21:21:29 +0300

    small mistype bugs

--- frontend/ediary_vue/src/mocks/dataEntryCards.ts
+++ frontend/ediary_vue/src/mocks/dataEntryCards.ts
@@ -5,7 +5,7 @@ export function getCards(): EntryCard[] {
     let cards: EntryCard[] = []
     for (const entry of entries) {
         cards.push(
-            { diaryId: entry.diaryId, entryId: entry.id, title: entry.title, 
+            { diaryId: entry.diaryID, entryId: entry.id, title: entry.title, 
                 scoreMood: Math.floor(Math.random() * 10) + 1, 
                 scoreProductivity: Math.floor(Math.random() * 10) + 1, 
                 createdDate: entry.createdDate }
