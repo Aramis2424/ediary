95e11255ba8640894779e39974bedbd923164630
Aramis2424 <srd2424@yandex.ru>
2025-07-08 12:38:59 +0300

    allow CORS from port 2525

--- frontend/ediary_vue/vite.config.ts
+++ frontend/ediary_vue/vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  server: {
+    port: 2525,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
