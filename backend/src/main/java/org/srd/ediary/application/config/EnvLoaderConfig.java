ed6f4b7363e0fa9ea3ee1466161bae7eb8ec1cda
Aramis2424 <srd2424@yandex.ru>
2025-07-23 17:06:51 +0300

    add dotEnv for load env files

---
+++ backend/src/main/java/org/srd/ediary/application/config/EnvLoaderConfig.java
@@ -0,0 +1,21 @@
+package org.srd.ediary.application.config;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EnvLoaderConfig {
+    static {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .filename("dev.env")
+                .load();
+        dotenv.entries().forEach(entry -> {
+            String key = entry.getKey();
+            String value = entry.getValue();
+
+            if (System.getProperty(key) == null && System.getenv(key) == null) {
+                System.setProperty(key, value);
+            }
+        });
+    }
+}
