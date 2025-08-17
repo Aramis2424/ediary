92d6fcd7d9143be44b137462371e959ecc83af2d
Aramis2424 <srd2424@yande.ru>
2025-08-17 12:57:29 +0300

    add methods to get entry cards for IT

--- backend/src/test/java/utils/EntryCardTestMother.java
+++ backend/src/test/java/utils/EntryCardTestMother.java
@@ -6,9 +6,20 @@ import java.time.LocalDate;
 
 public class EntryCardTestMother {
     public static EntryCardDTO getEntryCardDTO1(Long diaryId) {
-        return new EntryCardDTO(null, diaryId, "Day1", 1, 10, LocalDate.now());
+        return new EntryCardDTO(null, diaryId, "Day1",
+                1, 10, LocalDate.now());
     }
     public static EntryCardDTO getEntryCardDTO2(Long diaryId) {
-        return new EntryCardDTO(null, diaryId, "Day2", 2, 9, LocalDate.now());
+        return new EntryCardDTO(null, diaryId, "Day2",
+                2, 9, LocalDate.now());
+    }
+
+    public static EntryCardDTO getEntryCardDto1It(Long diaryId) {
+        return new EntryCardDTO(1L, diaryId, "Day1",
+                1, 10, LocalDate.of(2021, 1, 1));
+    }
+    public static EntryCardDTO getEntryCardDto2It(Long diaryId) {
+        return new EntryCardDTO(2L, diaryId, "Day2",
+                2, 9, LocalDate.of(2021, 1, 2));
     }
 }
