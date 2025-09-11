a0d68c8d8d9a6e38c4db721486e23ccff6316474
Aramis2424 <srd2424@yandex.ru>
2025-09-11 22:54:29 +0300

    implement default clearing file

--- metaData/fakeDataGenerator/src/main/java/org/srd/fakeDataGenerator/exporter/Exporter.java
+++ metaData/fakeDataGenerator/src/main/java/org/srd/fakeDataGenerator/exporter/Exporter.java
@@ -1,7 +1,16 @@
 package org.srd.fakeDataGenerator.exporter;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 import java.util.Map;
 
 public interface Exporter {
-    void export(String filename, Map<String, ?> content);
+    void export(String filename, List<Map<String, Object>> objects);
+
+    default void clearFile(Path path) throws IOException {
+        Files.write(path, new byte[0], StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+    }
 }
