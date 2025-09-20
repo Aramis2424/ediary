bf959da3b77f08e61a38ccf82defaa54abbc9723
Aramis2424 <srd2424@yandex.ru>
2025-09-20 16:07:17 +0300

    implement owners generation

--- metaData/fakeDataGenerator/src/main/java/org/srd/fakeDataGenerator/implementations/eDiary/GeneratorModelOwner.java
+++ metaData/fakeDataGenerator/src/main/java/org/srd/fakeDataGenerator/implementations/eDiary/GeneratorModelOwner.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Service;
 import org.srd.fakeDataGenerator.model.Model;
 import org.srd.fakeDataGenerator.service.GeneratorModel;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.*;
 
 @Service
 @Getter
@@ -25,7 +25,24 @@ public class GeneratorModelOwner extends GeneratorModel<ModelOwner> {
     }
 
     @Override
-    public List<ModelOwner> generate(Map<String, List<? extends Model>> context) {
-        return List.of();
+    public List<ModelOwner> generate(Map<String, List<?  extends Model>> context) {
+        List<ModelOwner> out = new ArrayList<>();
+        for (int id = 1; id <= baseGeneratedCount; id++) {
+            String name = faker.name().firstName();
+            LocalDate birthDate = LocalDate.ofInstant(
+                    faker.date().birthday(15, 30).toInstant(),
+                    ZoneId.systemDefault()
+            );
+            String login = faker.funnyName().name().replace(" ", "") + id;
+            String password = faker.lordOfTheRings().character().toLowerCase().replace(" ", "") +
+                    faker.number().numberBetween(100, 999);
+            LocalDate createdDate = LocalDate.of(
+                    faker.number().numberBetween(2020, 2024),
+                    faker.number().numberBetween(1, 12),
+                    faker.number().numberBetween(1, 28)
+            );
+            out.add(new ModelOwner(id, name, birthDate, login, password, createdDate));
+        }
+        return out;
     }
 }
