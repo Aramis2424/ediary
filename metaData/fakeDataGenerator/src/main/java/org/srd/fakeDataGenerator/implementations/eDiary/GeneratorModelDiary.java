ef5f28fb68630a31f66055a3d5c49ca4bcef7a99
Aramis2424 <srd2424@yandex.ru>
2025-09-20 20:21:10 +0300

    implement diaries generator

--- metaData/fakeDataGenerator/src/main/java/org/srd/fakeDataGenerator/implementations/eDiary/GeneratorModelDiary.java
+++ metaData/fakeDataGenerator/src/main/java/org/srd/fakeDataGenerator/implementations/eDiary/GeneratorModelDiary.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Service;
 import org.srd.fakeDataGenerator.model.Model;
 import org.srd.fakeDataGenerator.service.GeneratorModel;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Getter
@@ -26,6 +27,58 @@ public class GeneratorModelDiary extends GeneratorModel<ModelDiary> {
 
     @Override
     public List<ModelDiary> generate(Map<String, List<? extends Model>> context) {
-        return List.of();
+        List<? extends Model> owners = context.getOrDefault("owners", Collections.emptyList());
+        List<ModelDiary> out = new ArrayList<>();
+        int diaryId = 1;
+        for (Model model : owners) {
+            if (!(model instanceof ModelOwner owner)) {
+                continue;
+            }
+            int cntDiariesPerOwner = faker.number().numberBetween(0, 2);
+            for (int i = 0; i < cntDiariesPerOwner; i++) {
+                LocalDate createdDate = generateLocalDateAfter(owner.getCreatedDate(), 20);
+                out.add(
+                        new ModelDiary(
+                                diaryId,
+                                owner.getId(),
+                                generateSentence(3),
+                                generateSentence(5),
+                                faker.number().numberBetween(0, 5),
+                                createdDate
+                        )
+                );
+                diaryId++;
+            }
+        }
+        return out;
+    }
+
+    private String generateSentence(int maxWordsCount) {
+        int wordCount = random.nextInt(maxWordsCount) + 1;
+
+        return switch (wordCount) {
+            case 2 -> capitalizeFirst(faker.lorem().word() + " " + faker.lorem().word());
+            case 3 -> capitalizeFirst(faker.lorem().word() + " " +
+                    faker.lorem().word() + " " +
+                    faker.lorem().word());
+            default -> capitalizeFirst(faker.lorem().word());
+        };
+    }
+
+    private String capitalizeFirst(String text) {
+        if (text == null || text.isEmpty()) {
+            return text;
+        }
+        return Character.toUpperCase(text.charAt(0)) + text.substring(1);
+    }
+
+    private LocalDate generateLocalDateAfter(LocalDate afterDate, int maxDaysAfter) {
+        Date startDate = Date.from(afterDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        Date futureDate = faker.date().future(maxDaysAfter, TimeUnit.DAYS, startDate);
+
+        return futureDate.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
     }
 }
