f822610da33d18bdbcec82d9429f05b1e4ae87c9
Aramis2424 <srd2424@yandex.ru>
2025-11-02 21:07:40 +0300

    fix sizes

--- frontend/ediary_vue/src/views/EntriesSearching.vue
+++ frontend/ediary_vue/src/views/EntriesSearching.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="baseBacking">
-    <div class="bg-white rounded-xl p-2 w-1/6 h-1/6 gap-4 shadow-xl relative flex flex-col justify-evenly content-center items-center entriesSearching">
+    <div class="bg-white rounded-xl p-2 w-1/6 h-1/6 gap-2 shadow-xl relative flex flex-col justify-evenly content-center items-center entriesSearching">
         <h2 class="text-xl font-semibold mb-4">Поиск записей</h2>
         <SearchingField v-model="title" type="text" placeholder="Заголовок" label="Заголовок" />
         <SearchingField v-model="dateFrom" type="date" placeholder="С (включительно)" label="С" />
         <SearchingField v-model="dateTo" type="date" placeholder="По (включительно)" label="По" />
-        <button @click="applyFilters" class="baseBtn w-1/4">Применить</button>
+        <button @click="applyFilters" class="baseBtn w-32">Применить</button>
 
         <button @click="$emit('clicked')" class="absolute top-2 right-2 text-gray-500 hover:text-black">
             ✕
