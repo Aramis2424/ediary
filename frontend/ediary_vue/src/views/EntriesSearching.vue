e8f3eec95ea38847fbedffb23869318aa32ad1f0
Aramis2424 <srd2424@yandex.ru>
2025-11-02 20:00:34 +0300

    impl searching form

--- frontend/ediary_vue/src/views/EntriesSearching.vue
+++ frontend/ediary_vue/src/views/EntriesSearching.vue
@@ -1,8 +1,12 @@
 <template>
 <div class="baseBacking">
-    <div class="bg-white rounded-xl p-2 w-1/6 h-1/6 shadow-xl relative flex flex-col justify-evenly content-center items-center">
+    <div class="bg-white rounded-xl p-2 w-1/6 h-1/6 gap-4 shadow-xl relative flex flex-col justify-evenly content-center items-center entriesSearching">
         <h2 class="text-xl font-semibold mb-4">Поиск записей</h2>
-        <p>Пока не реализовано</p>
+        <SearchingField v-model="title" type="text" placeholder="Заголовок" label="Заголовок" />
+        <SearchingField v-model="dateFrom" type="date" placeholder="С (включительно)" label="С" />
+        <SearchingField v-model="dateTo" type="date" placeholder="По (включительно)" label="По" />
+        <button @click="applyFilters" class="baseBtn w-1/4">Применить</button>
+
         <button @click="$emit('clicked')" class="absolute top-2 right-2 text-gray-500 hover:text-black">
             ✕
         </button>
@@ -11,5 +15,20 @@
 </template>
 
 <script setup lang="ts">
-    const emit = defineEmits(["clicked"])
+import { ref } from 'vue'
+import SearchingField from '@/components/SearchingField.vue';
+
+const title = ref('')
+const dateFrom = ref('')
+const dateTo = ref('')
+
+const emit = defineEmits(['update:filters', "clicked"])
+
+function applyFilters() {
+    emit('update:filters', {
+        title: title.value || null,
+        dateFrom: dateFrom.value || null,
+        dateTo: dateTo.value || null
+    })
+}
 </script>
